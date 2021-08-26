98e45205cbf3eb3b6d61d5dae383fd8d58abfe6e
阿群 <xzqyymt@126.com>
2021-08-27 00:27:49 +0800

    week2

---
+++ week2/144.binary-tree-preorder-traversal.go
@@ -0,0 +1,16 @@
+package main
+
+func preorderTraversal(root *TreeNode) (res []int) {
+
+	// nil 返回空
+
+	if root == nil {
+		return nil
+	}
+	// 根左右
+	res = append(res, root.Val)
+	res = append(res, preorderTraversal(root.Left)...)
+	res = append(res, preorderTraversal(root.Right)...)
+	return
+
+}
---
+++ week2/429.n-ary-tree-level-order-traversal.go
@@ -0,0 +1,10 @@
+package main
+
+import "container/list"
+
+//  BFS
+func levelOrder(root *Node) (res [][]int) {
+
+	list.New
+
+}
